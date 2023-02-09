487c4e1cd7422f7908e94319ae23e59d106792ab
Eon4 <ladyforest85@gmail.com>
2023-02-09 09:21:48 +0100

    Update arrays.js

--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -1,36 +1,14 @@
-
-//Fra tidligere gennemgang 
-// let myArray=[1,2,3,4,5,6,7,8,9];
-
-// let myArray=["bo","mads", "mikkel"];
-
-
-// console.log(myArray);
-// console.log(myArray[2]);
-
-
-// myArray.push("niklas","simon");
-
-// console.log(myArray[1]);
-
-
-// myArray.pop();
-// myArray.shift();
-// myArray.unshift("simon");
-
-
-// console.log(myArray);
-
-/* arrays */
-
+/* arrays */ 
 //Opgave 1
 const myArray = ['element1','element2','element3','element4','element5'];
 
 console.log(myArray);
 
-
-
 // opgave 2 console.logs
 
-
+console.log(myArray[2]);
+console.log(myArray[4]);
+console.log(myArray[0]);
+console.log(myArray[3]);
+console.log(myArray[1]);
 
